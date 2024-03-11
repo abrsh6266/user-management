5ec92c7a5e2a6516020044c2354d68e0eebf60e5
Abrham <abrsh6266@gmail.com>
2024-03-10 23:00:25 -0800

    some updates

--- models/User.js
+++ models/User.js
@@ -3,22 +3,25 @@ import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    username:{
-        type: String,
-        required: true
-    },
-    name:{
-        type: String,
-        required: true
-    },
-    email:{
-        type: String,
-        required: true,
-        unique: true
-    },
-    password:{
-        type: String,
-        required: true
-    },
+  username: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
 
-})
+const User = mongoose.model("User", userSchema);
+
+export default User;
