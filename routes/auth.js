1c81c8ff29791f3d22f0c7afb7e552a3a6ae2f7e
Abrham <abrsh6266@gmail.com>
2024-03-11 05:08:43 -0800

    creating login route

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,43 @@
 import { Router } from "express";
 import User from "../models/user";
 import bcryptjs from "bcryptjs";
-import {signUpBodyValidation} from "../utils/validationSchema"
+import {
+  signUpBodyValidation,
+  logInBodyValidation,
+} from "../utils/validationSchema";
 const router = Router();
 
 router.post("/signup", async (req, res) => {
+  try {
+    const { error } = logInBodyValidation(req.body);
+    if (error) {
+      return res
+        .status(400)
+        .json({ error: true, message: error.details[0].message });
+    }
+    const user = await User.findOne({ email: req.body.email });
+    if (user) {
+      return res
+        .status(400)
+        .json({ error: true, message: "Invalid email or password" });
+    }
+    if (!verifiedPassword) {
+      return res
+        .status(400)
+        .json({ error: true, message: error.details[0].message });
+    }
+
+    const verifiedPassword = await bcryptjs.compare(
+      req.body.password,
+      user.password
+    );
+    
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: true, message: "Internal Server Error" });
+  }
+});
+router.post("/login", async (req, res) => {
   try {
     const { error } = signUpBodyValidation(req.body);
     if (error) {
@@ -30,5 +63,4 @@ router.post("/signup", async (req, res) => {
     res.status(500).json({ error: true, message: "Internal Server Error" });
   }
 });
-
 export default router;
