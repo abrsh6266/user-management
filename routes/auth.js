7360797b2aac7985648d0202cf3f81f48f0e3f76
Abrham <abrsh6266@gmail.com>
2024-03-11 04:57:19 -0800

    some updates

--- routes/auth.js
+++ routes/auth.js
@@ -1,3 +1,43 @@
 import { Router } from "express";
-
 import User from "../models/user";
+import bcryptjs from "bcryptjs";
+import Joi from "joi";
+import passwordComplexity from "joi-password-complexity";
+const router = Router();
+
+router.post("/signup", async (req, res) => {
+  try {
+    const { error } = signUpBodyValidation(req.body);
+    if (error) {
+      return res
+        .status(400)
+        .json({ error: true, message: error.details[0].message });
+    }
+    const user = await User.findOne({ email: req.body.email });
+    if (user) {
+      return res
+        .status(400)
+        .json({ error: true, message: "user with given email already exist!" });
+    }
+    const salt = await bcryptjs.genSalt(Number(process.env.SALT));
+    const hashPassword = await bcryptjs.hash(req.body.password, salt);
+
+    await new User({ ...req.body, password: hashPassword }).save();
+    res
+      .status(201)
+      .json({ error: false, message: "Account created successfully!" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: true, message: "Internal Server Error" });
+  }
+});
+const signUpBodyValidation = (body) => {
+  const schema = Joi.object({
+    username: Joi.string().required().label("User Name"),
+    email: Joi.string().email().required().label("Email"),
+    password: passwordComplexity().required().label("Password"),
+  });
+  return schema.validate(body);
+};
+
+export default router;
