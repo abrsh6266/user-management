12f3e2e25de8dc9a628dd5768fe5efcc74cfea95
Abrham <abrsh6266@gmail.com>
2024-03-11 20:33:07 -0800

    get all posts endpoint

--- routes/post.js
+++ routes/post.js
@@ -9,23 +9,36 @@ import {
 const router = Router();
 
 router.post("/create", isLoggedIn, async (req, res) => {
-    try {
-      const { error } = createPostValidation.validate(req.body);
-      if (error) {
-        return res.status(400).json({ error: true, message: error.details[0].message });
-      }
-  
-      const { title, content } = req.body;
-      const newPost = new Post({
-        title,
-        content,
-        author: req.user._id,
-      });
-      await newPost.save();
-      res.status(201).json({ error: false, message: "Post created successfully" });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: true, message: "Internal Server Error" });
+  try {
+    const { error } = createPostValidation.validate(req.body);
+    if (error) {
+      return res
+        .status(400)
+        .json({ error: true, message: error.details[0].message });
     }
-  });
-  export default router;
+
+    const { title, content } = req.body;
+    const newPost = new Post({
+      title,
+      content,
+      author: req.user._id,
+    });
+    await newPost.save();
+    res
+      .status(201)
+      .json({ error: false, message: "Post created successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: true, message: "Internal Server Error" });
+  }
+});
+router.get("/get", isLoggedIn, async (req, res) => {
+  try {
+    const posts = await Post.find().populate("author", "username");
+    res.status(200).json({ error: false, posts });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: true, message: "Internal Server Error" });
+  }
+});
+export default router;
