bbd98689c87c0a4f95677d51d4f89deddd64f609
Abrham <abrsh6266@gmail.com>
2024-03-11 20:06:15 -0800

    creating create post endpoint

--- routes/post.js
+++ routes/post.js
@@ -1,5 +1,28 @@
 import { Router } from "express";
-import User from "../models/user.js";
+import Post from "../models/Post.js";
 import isLoggedIn from "../middleware/isLoggedIn.js";
-
+import {
+  createPostValidation,
+  fetchPostValidation,
+} from "../utils/validationSchema.js";
+router.post("/create", isLoggedIn, async (req, res) => {
+    try {
+      const { error } = createPostValidation.validate(req.body);
+      if (error) {
+        return res.status(400).json({ error: true, message: error.details[0].message });
+      }
+  
+      const { title, content } = req.body;
+      const newPost = new Post({
+        title,
+        content,
+        author: req.user._id,
+      });
+      await newPost.save();
+      res.status(201).json({ error: false, message: "Post created successfully" });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: true, message: "Internal Server Error" });
+    }
+  });
 const router = Router();
